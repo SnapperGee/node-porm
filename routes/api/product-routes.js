74f3c6ea499bc294d85c810b3536024fb39bb173
Snap <XxSnapperGeexX@gmail.com>
2023-11-02 12:26:27 -0700

    Implement Product get and delete routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try
+  {
+    const products = await Product.findAll({include: [
+      {model: Category}, {model: Tag}
+    ]});
+    res.json(products);
+  } catch (error)
+  {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try
+  {
+    const products = await Product.findByPk(req.params.id, {include: [
+      {model: Category},
+      {model: Tag}
+    ]});
+    res.json(products);
+  } catch (error)
+  {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
@@ -38,7 +59,7 @@ router.post('/', (req, res) => {
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      res.json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
@@ -57,7 +78,7 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-        
+
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
@@ -92,8 +113,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const numOfDeleted = await Product.destroy({where: { id: parseInt(req.params.id) }});
+  res.json(numOfDeleted);
 });
 
 module.exports = router;
