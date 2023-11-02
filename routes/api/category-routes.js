ef2db948ba5396326ee0df582483368bf17d1fef
Snap <XxSnapperGeexX@gmail.com>
2023-11-02 13:55:58 -0700

    Implement category delete route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,12 +17,21 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try
+  {
+    const numOfDeleted = await Category.destroy({where: { id: parseInt(req.params.id) }});
+    res.json(numOfDeleted);
+  }
+  catch (error)
+  {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
