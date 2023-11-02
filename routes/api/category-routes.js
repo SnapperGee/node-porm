275c5ceb63f2007e7b41d2498b5db2cc21e84679
Snap <XxSnapperGeexX@gmail.com>
2023-11-02 14:10:38 -0700

    Implement category get by id route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,9 +19,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try
+  {
+    const product = await Category.findByPk(req.params.id, {
+      include: [
+        {model: Product}
+    ]});
+    res.json(product);
+  }
+  catch (error)
+  {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', async (req, res) => {
