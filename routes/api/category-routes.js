7becf4482357ea358f3378a8f5d70a708db068fe
Snap <XxSnapperGeexX@gmail.com>
2023-11-02 13:56:19 -0700

    Implement category update route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,15 @@ router.post('/', (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try
+  {
+    const tag = await Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}});
+    res.json(tag);
+  }
+  catch (error)
+  {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
